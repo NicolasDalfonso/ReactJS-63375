d540857f7501a44cba76bddb14e724f1070d975f
DalfonsoNicolas <dalfonsonicolas@gmail.com>
2024-10-30 17:10:29 -0300

    Preentrega1

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import Navbar from './components/NavBar/NavBar'
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <ItemListContainer/>
+    </>
+  )
+}
+
+export default App
